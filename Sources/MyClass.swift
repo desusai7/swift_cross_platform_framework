beee5bf4dec37658e05f702dff21333bce6fe526
Desu Sai Venkat <saivenkatdesu@gmail.com>
2021-10-18 10:25:36 +0530

    Cross Platform Framework for iOS, WatchOS, TvOS

---
+++ Sources/MyClass.swift
@@ -0,0 +1,14 @@
+//
+//  MyClass.swift
+//  Rudder
+//
+//  Created by Desu Sai Venkat on 18/10/21.
+//
+
+import Foundation
+
+public class MyClass {
+    public class func printTypeName() {
+        print ("Rudder")
+    }
+}
---
+++ Sources/Rudder.h
@@ -0,0 +1,17 @@
+//
+//  Rudder.h
+//  Rudder
+//
+//  Created by Desu Sai Venkat on 18/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Rudder.
+FOUNDATION_EXPORT double Rudder_VersionNumber;
+
+//! Project version string for Rudder
+FOUNDATION_EXPORT const unsigned char Rudder_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Rudder/PublicHeader.h>
+
